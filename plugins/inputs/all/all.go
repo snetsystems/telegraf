7fefb54979251e9a1ce3dbfb81df48a75f4e9348
yongsikgi <yongsikgi@gmail.com>
2023-05-26 14:46:22 +0900

    Annotated the oracledb input plugin

--- plugins/inputs/all/all.go
+++ plugins/inputs/all/all.go
@@ -146,7 +146,8 @@ import (
 	_ "github.com/influxdata/telegraf/plugins/inputs/openstack"
 	_ "github.com/influxdata/telegraf/plugins/inputs/opentelemetry"
 	_ "github.com/influxdata/telegraf/plugins/inputs/openweathermap"
-	_ "github.com/influxdata/telegraf/plugins/inputs/oracledb"
+
+	// _ "github.com/influxdata/telegraf/plugins/inputs/oracledb"
 	_ "github.com/influxdata/telegraf/plugins/inputs/passenger"
 	_ "github.com/influxdata/telegraf/plugins/inputs/pf"
 	_ "github.com/influxdata/telegraf/plugins/inputs/pgbouncer"
